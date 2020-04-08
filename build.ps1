bb5aee02213ea3f4ef6dcfad07493be694c5b05a
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-09 08:10:50 +0900

    Add build script

--- build.ps1
+++ build.ps1
@@ -4,12 +4,10 @@ $runtimes = @(
   @("osx-x64", ""),
   @("linux-x64", "")
 )
-$xml = [xml](Get-Content ghacu.nuspec)
+$xml = [xml](Get-Content chocolatey\ghacu.nuspec)
 $version = $xml.package.metadata.version
 For ($i = 0; $i -lt $runtimes.Length; $i++) {
   & dotnet build -c Release -r $($runtimes[$i][0]) -p:Version=$version -o bin\$($runtimes[$i][0])
-  Get-ChildItem -Path $('bin\' + $runtimes[$i][0]) -Recurse -exclude $('ghacu' + $runtimes[$i][1]) |
-    Select -ExpandProperty FullName |
-    sort length -Descending |
-    Remove-Item -force
+  Move-Item -Path $('bin\' + $runtimes[$i][0] + '\ghacu' + $runtimes[$i][1]) -Destination $('bin\ghacu.' + $runtimes[$i][0] + $runtimes[$i][1])
+  Remove-Item $('bin\' + $runtimes[$i][0]) -Recurse
 }
