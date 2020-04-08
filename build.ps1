df76a3a25d3eb9bdc9816de959b11cf1370d98c8
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-09 08:16:46 +0900

    Change dot to dash in binaries and add version

--- build.ps1
+++ build.ps1
@@ -8,6 +8,6 @@ $xml = [xml](Get-Content chocolatey\ghacu.nuspec)
 $version = $xml.package.metadata.version
 For ($i = 0; $i -lt $runtimes.Length; $i++) {
   & dotnet build -c Release -r $($runtimes[$i][0]) -p:Version=$version -o bin\$($runtimes[$i][0])
-  Move-Item -Path $('bin\' + $runtimes[$i][0] + '\ghacu' + $runtimes[$i][1]) -Destination $('bin\ghacu.' + $runtimes[$i][0] + $runtimes[$i][1])
+  Move-Item -Path $('bin\' + $runtimes[$i][0] + '\ghacu' + $runtimes[$i][1]) -Destination $('bin\ghacu-' + $version + '-' + $runtimes[$i][0] + $runtimes[$i][1])
   Remove-Item $('bin\' + $runtimes[$i][0]) -Recurse
 }
