7b0521b6fbb62083add59e697b1d556fb5f4df9f
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-08 21:59:50 +0900

    Add executable files

--- build.ps1
+++ build.ps1
@@ -4,9 +4,11 @@ $runtimes = @(
   @("osx-x64", ""),
   @("linux-x64", "")
 )
+$xml = [xml](Get-Content ghacu.nuspec)
+$version = $xml.package.metadata.version
 For ($i = 0; $i -lt $runtimes.Length; $i++) {
-  & dotnet build -c Release -r $($runtimes[$i][0]) -p:Version=1.1.0 -o bin\Release\$($runtimes[$i][0])
-  Get-Children -Path 'bin\Release\'$runtimes[$i][0] -Recurse -exclude 'ghacu'$runtimes[$i][1] |
+  & dotnet build -c Release -r $($runtimes[$i][0]) -p:Version=$version -o bin\$($runtimes[$i][0])
+  Get-ChildItem -Path $('bin\' + $runtimes[$i][0]) -Recurse -exclude $('ghacu' + $runtimes[$i][1]) |
     Select -ExpandProperty FullName |
     sort length -Descending |
     Remove-Item -force
