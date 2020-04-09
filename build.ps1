7d3ffc784ae08fc20aae130073d9c22bf450f104
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-09 13:51:32 +0900

    Fix build script

--- build.ps1
+++ build.ps1
@@ -1,13 +1,25 @@
 $runtimes = @(
-  @("win-x86", ".exe"),
-  @("win-x64", ".exe"),
+  @("linux-x64", ""),
   @("osx-x64", ""),
-  @("linux-x64", "")
+  @("win-x64", ".exe"),
+  @("win-x86", ".exe")
 )
+# $exclude = @(
+#   'CommandLine.dll',
+#   'ghacu.deps.json',
+#   'ghacu.dll',
+#   'ghacu.pdb',
+#   'ghacu.runtimeconfig.json',
+#   'LiteDB.dll',
+#   'Octokit.dll',
+#   'YamlDotNet.dll'
+# )
 $xml = [xml](Get-Content chocolatey\ghacu.nuspec)
 $version = $xml.package.metadata.version
 For ($i = 0; $i -lt $runtimes.Length; $i++) {
   & dotnet build -c Release -r $($runtimes[$i][0]) -p:Version=$version -o bin\$($runtimes[$i][0])
-  Move-Item -Path $('bin\' + $runtimes[$i][0] + '\ghacu' + $runtimes[$i][1]) -Destination $('bin\ghacu-' + $version + '-' + $runtimes[$i][0] + $runtimes[$i][1])
-  Remove-Item $('bin\' + $runtimes[$i][0]) -Recurse
+  # Get-ChildItem -Path $('bin\' + $runtimes[$i][0]) -Recurse -exclude $($exclude + $('ghacu' + $runtimes[$i][1])) |
+  #   Select -ExpandProperty FullName |
+  #   sort length -Descending |
+  #   Remove-Item -force
 }
