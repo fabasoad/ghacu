4b0da5102232b65a19785429a4c0723a933e7ee1
fabasoad <fabasoad@gmail.com>
2020-07-13 22:35:29 +0900

    #14 - Add uninstall scripts.

---
+++ scripts/uninstall-ghacu-linux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+unlink /usr/local/bin/ghacu
+rm -rf ~/ghacu-2.0.1-linux-x64/
+rm -r ~/ghacu-2.0.1-linux-x64.tgz
---
+++ scripts/uninstall-ghacu-osx.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+unlink /usr/local/bin/ghacu
+rm -rf ~/ghacu-2.0.1-osx-x64/
+rm -r ~/ghacu-2.0.1-osx-x64.tgz
