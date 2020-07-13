9b81532ed2f0c0f4fcbf9dac3ea73e6533ac442d
fabasoad <fabasoad@gmail.com>
2020-07-13 22:09:24 +0900

    #14 - Change path to /usr/local/bin/ghacu

--- scripts/install-ghacu-osx.sh
+++ scripts/install-ghacu-osx.sh
@@ -2,4 +2,4 @@
 cd ~ && wget https://github.com/fabasoad/ghacu/releases/download/v2.0.1/ghacu-2.0.1-osx-x64.tgz
 tar -xvf ghacu-2.0.1-osx-x64.tgz
 chmod +x ~/ghacu-2.0.1-osx-x64/ghacu
-ln -s ~/ghacu-2.0.1-osx-x64/ghacu /usr/bin/ghacu
+ln -s ~/ghacu-2.0.1-osx-x64/ghacu /usr/local/bin/ghacu
