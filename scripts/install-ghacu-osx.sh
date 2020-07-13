4b9dad4d81e842c5b55f2f203afdfe8da6e506f6
fabasoad <fabasoad@gmail.com>
2020-07-13 21:51:59 +0900

    #14 - Create 2 installation scripts

---
+++ scripts/install-ghacu-osx.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd ~ && wget https://github.com/fabasoad/ghacu/releases/download/v2.0.1/ghacu-2.0.1-osx-x64.tgz
+tar -xvf ghacu-2.0.1-osx-x64.tgz
+chmod +x ~/ghacu-2.0.1-osx-x64/ghacu
+ln -s ~/ghacu-2.0.1-osx-x64/ghacu /usr/bin/ghacu
