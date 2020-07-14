fe4bebcc4c0d77acc3ebbc1762d3d5a1c8cdb2f2
fabasoad <fabasoad@gmail.com>
2020-07-14 22:29:58 +0900

    #14 - Add set PATH

--- scripts/install-ghacu-winx64.ps1
+++ scripts/install-ghacu-winx64.ps1
@@ -1,3 +1,7 @@
 Invoke-WebRequest -Uri https://github.com/fabasoad/ghacu/releases/download/v2.0.1/ghacu-2.0.1-win-x64.tgz -OutFile ~\ghacu-2.0.1-win-x64.tgz
-tar -xf ~\ghacu-2.0.1-win-x64.tgz
-New-Item -ItemType SymbolicLink -Path "~\ghacu-2.0.1-win-x64\ghacu.exe" -Target "C:\Windows\System32\ghacu.exe"
+cd ~ && tar -xf ghacu-2.0.1-win-x64.tgz
+Rename-Item .\ghacu-2.0.1-win-x64 .\ghacu
+$PathGhacu = (Resolve-Path ~)
+$PathEnvOld = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+$PathEnvNew = “$PathEnvOld;$PathGhacu”
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $PathEnvNew
