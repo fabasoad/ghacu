5ac62614896d00622b3e8ec64e2fd4514f89e851
fabasoad <fabasoad@gmail.com>
2020-07-15 22:04:37 +0900

    #14 - ln -sfn instead of ln -s

--- scripts/bash/install-ghacu-linux.sh
+++ scripts/bash/install-ghacu-linux.sh
@@ -4,5 +4,4 @@ version='2.0.1'
 cd ~ && wget "https://github.com/fabasoad/ghacu/releases/download/v$version/ghacu-$version-$os.tgz"
 tar -xvf "ghacu-$version-$os.tgz"
 chmod +x "~/ghacu-$version-$os/ghacu"
-unlink "/usr/local/bin/ghacu"
-ln -s "~/ghacu-$version-$os/ghacu" "/usr/local/bin/ghacu"
+ln -sfn "~/ghacu-$version-$os/ghacu" "/usr/local/bin/ghacu"
