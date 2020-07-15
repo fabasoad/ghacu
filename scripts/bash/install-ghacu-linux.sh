4d90121a0018961dd2e35e1113bd2d241da5fbf6
fabasoad <fabasoad@gmail.com>
2020-07-15 22:32:30 +0900

    #14 - Fix issue with double quotes in path

--- scripts/bash/install-ghacu-linux.sh
+++ scripts/bash/install-ghacu-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 os='linux-x64'
 version='2.0.1'
-cd ~ && wget "https://github.com/fabasoad/ghacu/releases/download/v$version/ghacu-$version-$os.tgz"
-tar -xvf "ghacu-$version-$os.tgz"
-chmod +x "~/ghacu-$version-$os/ghacu"
-ln -sfn "~/ghacu-$version-$os/ghacu" "/usr/local/bin/ghacu"
+cd ~ && wget https://github.com/fabasoad/ghacu/releases/download/v$version/ghacu-$version-$os.tgz
+tar -xvf ghacu-$version-$os.tgz
+chmod +x ~/ghacu-$version-$os/ghacu
+ln -sfn ~/ghacu-$version-$os/ghacu /usr/local/bin/ghacu
