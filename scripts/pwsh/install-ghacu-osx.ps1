b4dd6d8a8934e519e42885827d13b41dd2ab2ce4
fabasoad <fabasoad@gmail.com>
2020-07-15 22:57:32 +0900

    #14 - Fix pwsh scripts

--- scripts/pwsh/install-ghacu-osx.ps1
+++ scripts/pwsh/install-ghacu-osx.ps1
@@ -3,4 +3,4 @@ $version = '2.0.1'
 Invoke-WebRequest -Uri "https://github.com/fabasoad/ghacu/releases/download/v$version/ghacu-$version-$os.tgz" -OutFile "~\ghacu-$version-$os.tgz"
 cd ~ && tar -xf "ghacu-$version-$os.tgz"
 $path = (Resolve-Path ~)
-New-Item -ItemType SymbolicLink -Path /usr/local/bin/ghacu -Value $path/ghacu-$verion-$os/ghacu -Force
+New-Item -ItemType SymbolicLink -Path /usr/local/bin/ghacu -Value $path/ghacu-$version-$os/ghacu -Force
