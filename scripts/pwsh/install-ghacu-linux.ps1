cca3f3725b7d4840c426a069bc16c0060151086a
fabasoad <fabasoad@gmail.com>
2020-07-15 23:06:17 +0900

    #14 - Fix pwsh scripts

--- scripts/pwsh/install-ghacu-linux.ps1
+++ scripts/pwsh/install-ghacu-linux.ps1
@@ -1,6 +1,6 @@
-$os = 'ubuntu-x64'
 $version = '2.0.1'
-Invoke-WebRequest -Uri "https://github.com/fabasoad/ghacu/releases/download/v$version/ghacu-$version-$os.tgz" -OutFile "~\ghacu-$version-$os.tgz"
+$os = 'ubuntu-x64'
+Invoke-WebRequest -Uri "https://github.com/fabasoad/ghacu/releases/download/v$version/ghacu-$version-$os.tgz" -OutFile "~/ghacu-$version-$os.tgz"
 cd ~ && tar -xf "ghacu-$version-$os.tgz"
 $path = (Resolve-Path ~)
 New-Item -ItemType SymbolicLink -Path /usr/local/bin/ghacu -Value $path/ghacu-$version-$os/ghacu -Force
--- scripts/pwsh/install-ghacu-winx64.ps1
+++ scripts/pwsh/install-ghacu-winx64.ps1
@@ -1,6 +1,6 @@
-$os = 'win-x64'
 $version = '2.0.1'
+$os = 'win-x64'
 Invoke-WebRequest -Uri "https://github.com/fabasoad/ghacu/releases/download/v$version/ghacu-$version-$os.tgz" -OutFile "~\ghacu-$version-$os.tgz"
 cd ~ && tar -xf "ghacu-$version-$os.tgz"
 $path = (Resolve-Path ~)
-New-Item -ItemType SymbolicLink -Path /usr/local/bin/ghacu -Value $path/ghacu-$version-$os/ghacu -Force
+New-Item -ItemType SymbolicLink -Path C:\Windows\System32\ghacu.exe -Value $path\ghacu-$version-$os\ghacu.exe -Force
