26632a61dad2207201b4f607ef4647c87264f77f
fabasoad <fabasoad@gmail.com>
2020-07-21 12:08:26 +0900

    Fix ps1 scripts

--- scripts/install-ghacu-winx64.ps1
+++ scripts/install-ghacu-winx64.ps1
@@ -9,9 +9,10 @@ For ($i = 0; $i -lt $release.assets.Length; $i++)
         break
     }
 }
-Invoke-WebRequest -Uri $asset.browser_download_url -OutFile "~\${asset.name}"
-cd ~ && tar -xf $asset.name
-$folder = [io.path]::GetFileNameWithoutExtension($asset.name)
+$asset_name = $asset.name
+Invoke-WebRequest -Uri $asset.browser_download_url -OutFile "~\$asset_name"
+cd ~ && tar -xf $asset_name
+$folder = [io.path]::GetFileNameWithoutExtension($asset_name)
 $path = (Resolve-Path ~)
 $env:Path += ";$path\$folder"
 New-Item -ItemType SymbolicLink -Path C:\Windows\System32\ghacu.exe -Value $path\$folder\ghacu.exe -Force
